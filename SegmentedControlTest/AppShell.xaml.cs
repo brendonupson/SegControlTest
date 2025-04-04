d08ac48853ae6d9ed0819cc77af7b8e9740804bf
brendonupson <bupson@wnc.net.au>
2025-04-04 20:25:20 +1300

    Init

---
+++ SegmentedControlTest/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SegmentedControlTest;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SegmentedControlTest/Tab2Page.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using SegmentedControlTest.Views;
+namespace SegmentedControlTest;
+
+public partial class Tab2Page : ContentPage
+{
+
+	public Tab2Page()
+	{
+		InitializeComponent();
+	}
+
+
+}
+
